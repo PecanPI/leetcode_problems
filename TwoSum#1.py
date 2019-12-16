e5d715f44cd8be7fba484b677c9e746989eb0b9d
PecanPI <mloga407@gmail.com>
2019-12-16 06:42:06 -0700

    Answered Two Sum

--- TwoSum#1.py
+++ TwoSum#1.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
