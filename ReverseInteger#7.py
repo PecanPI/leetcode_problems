6b6a5a649dd3fae938111d1132c7a09fc4168d25
PecanPI <mloga407@gmail.com>
2019-12-18 06:45:30 -0700

    finished reverse integer

--- ReverseInteger#7.py
+++ ReverseInteger#7.py
@@ -1,15 +1,22 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x >= 0:
-            string_int = str(x)
-            reverse_int = string_int[::-1]
-            return int(reverse_int)
-        else:
-            string_int = str(x)
-            reverse_int = string_int[:0:-1]
-            print(reverse_int)
-            return -1 * int(reverse_int)
+            if x >= 0:
+                string_int = str(x)
+                reverse_int = string_int[::-1]
+                if int(reverse_int) < (2**31 - 1):
+                    return int(reverse_int)
+                else:
+                    return 0
+            else:
+                string_int = str(x)
+                reverse_int = string_int[:0:-1]
+                if -1 * int(reverse_int) > (-(2 ** 31)):
+                    return -1 * int(reverse_int)
+                else:
+                    return 0
+
+
 
 
 sol = Solution()
-print(sol.reverse(-123))
+print(sol.reverse(-2147483648))
