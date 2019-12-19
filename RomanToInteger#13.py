49b32f37f1d14c3a46f569f2929da45d6a71e426
PecanPI <mloga407@gmail.com>
2019-12-19 06:40:46 -0700

    added and finished RomanToInteger#13.py

---
+++ RomanToInteger#13.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman_convert= {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_subtract = {'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}
+        num = 0
+        for i in s:
+            num += roman_convert[i]
+        for i in roman_subtract:
+            if i in s:
+                num += roman_subtract[i]
+
+        return num
+
+
+sol = Solution()
+print(sol.romanToInt('XLIII'))
